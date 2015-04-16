ceffb4ed9de812e36b8262c23d3bab60a5048208
Andrew Robinson <SirArkimedes@gmail.com>
2015-04-16 08:47:13 -0500

    Add Collection View

---
+++ Andrew Robinson/AboutMeCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  AboutMeCollectionView.h
+//  Andrew Robinson
+//
+//  Created by Andrew Robinson on 4/16/15.
+//  Copyright (c) 2015 Andrew Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutMeCollectionView : UICollectionView
+
+@end
---
+++ Andrew Robinson/AboutMeCollectionView.m
@@ -0,0 +1,21 @@
+//
+//  AboutMeCollectionView.m
+//  Andrew Robinson
+//
+//  Created by Andrew Robinson on 4/16/15.
+//  Copyright (c) 2015 Andrew Robinson. All rights reserved.
+//
+
+#import "AboutMeCollectionView.h"
+
+@implementation AboutMeCollectionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
