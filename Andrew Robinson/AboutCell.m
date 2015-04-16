47b47640794b6d1be32e1751eabb363c8c3df0f1
Andrew Robinson <SirArkimedes@gmail.com>
2015-04-16 09:47:51 -0500

    Made a custom cell

---
+++ Andrew Robinson/AboutCell.m
@@ -0,0 +1,37 @@
+//
+//  AboutCell.m
+//  Andrew Robinson
+//
+//  Created by Andrew Robinson on 4/16/15.
+//  Copyright (c) 2015 Andrew Robinson. All rights reserved.
+//
+
+#import "AboutCell.h"
+
+@implementation AboutCell
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        // Initialization code
+        NSArray *arrayOfViews = [[NSBundle mainBundle] loadNibNamed:@"AboutCell" owner:self options:nil];
+        
+        if ([arrayOfViews count] < 1) {
+            return nil;
+        }
+        
+        if (![[arrayOfViews objectAtIndex:0] isKindOfClass:[UICollectionViewCell class]]) {
+            return nil;
+        }
+        
+        self = [arrayOfViews objectAtIndex:0];
+        
+    }
+    
+    return self;
+    
+}
+
+@end
--- Andrew Robinson/MainViewController.m
+++ Andrew Robinson/MainViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MainViewController.h"
+#import "AboutCell.h"
 
 @interface MainViewController ()
 
