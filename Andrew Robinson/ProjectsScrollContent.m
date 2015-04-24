ac10579f5b8bc2eb34567fff5e6b21b161d7c16d
Andrew Robinson <SirArkimedes@gmail.com>
2015-04-24 12:07:55 -0500

    Add GPA Cal video

--- Andrew Robinson/ProjectsScrollContent.m
+++ Andrew Robinson/ProjectsScrollContent.m
@@ -8,7 +8,52 @@
 
 #import "ProjectsScrollContent.h"
 
+@interface ProjectsScrollContent ()
+
+@property (strong, nonatomic) MPMoviePlayerController *videoController;
+@property (weak, nonatomic) IBOutlet UIView *videoView;
+
+@end
+
 @implementation ProjectsScrollContent
+
+- (IBAction)playGPACal:(id)sender {
+    
+    [self movieView];
+    
+}
+
+- (void)movieView {
+    
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSString *moviePath = [bundle pathForResource:@"GPA Cal Video" ofType:@"mov"];
+    NSURL *movieURL = [NSURL fileURLWithPath:moviePath];
+    
+    self.videoController = [[MPMoviePlayerController alloc] init];
+    
+    [self.videoController setContentURL:movieURL];
+    [self.videoController.view setFrame:self.videoView.frame];
+    [self addSubview:self.videoController.view];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(videoPlayBackDidFinish:)
+                                                 name:MPMoviePlayerPlaybackDidFinishNotification
+                                               object:self.videoController];
+    
+    [self.videoController play];
+    
+}
+
+- (void)videoPlayBackDidFinish:(NSNotification *)notification {
+    
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:nil];
+    
+    // Stop the video player and remove it from view
+    [self.videoController stop];
+    [self.videoController.view removeFromSuperview];
+    self.videoController = nil;
+    
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
